e340f35be17045b34235abe3f32d9341a71a707f
github-action-benchmark <github@users.noreply.github.com>
2021-06-25 05:24:14 +0000

    add Benchmark (pytest) benchmark result for b471a570e9db41d7ea00b51ca499113d50fe29a9

---
+++ dev/bench/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1624598654301,
+  "repoUrl": "https://github.com/xNok/Using-Continuous-Testing-in-DevOps-Workflows",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "nokwebspace@gmail.com",
+            "name": "xNok",
+            "username": "xNok"
+          },
+          "committer": {
+            "email": "nokwebspace@gmail.com",
+            "name": "xNok",
+            "username": "xNok"
+          },
+          "distinct": true,
+          "id": "b471a570e9db41d7ea00b51ca499113d50fe29a9",
+          "message": "more option for performance test",
+          "timestamp": "2021-06-25T01:08:20-04:00",
+          "tree_id": "3fd3294b2fabb3cec5e565164aaa051c0e2a8ece",
+          "url": "https://github.com/xNok/Using-Continuous-Testing-in-DevOps-Workflows/commit/b471a570e9db41d7ea00b51ca499113d50fe29a9"
+        },
+        "date": 1624598653461,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests_performances/test_benchmark.py::test_my_stuff",
+            "value": 14798.314084009155,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00004039028467588044",
+            "extra": "mean: 67.57526528515743 usec\nrounds: 12643"
+          }
+        ]
+      }
+    ]
+  }
+}
